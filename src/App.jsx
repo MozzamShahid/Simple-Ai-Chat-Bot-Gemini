338423b01671b173ada42ef14aee4cd9c2948e51
Mozzam Shahid <mozzam@Mozzams-MacBook-Air.local>
2024-09-17 17:34:50 +0500

    newpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
 import Home from './pages/Home';
 import ImagePage from './pages/[imageId]';
-
-
-import './App.css';
-import './index.css';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
-        {/* Header will be present on all pages */}
-        <Header />
-
-        {/* Routes for different pages */}
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/:imageId" element={<ImagePage />} />  {/* Dynamic Route */}
-
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:imageId" element={<ImagePage />} />  {/* Dynamic Route */}
+      </Routes>
     </Router>
   );
 }
