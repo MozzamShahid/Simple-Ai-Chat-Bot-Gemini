09a7799287af55209c29ce681d7b37aaa0ac0147
Mozzam Shahid <mozzam@Mozzams-MacBook-Air.local>
2024-09-17 17:50:53 +0500

    Add vercel.json for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import ImagePage from './pages/ImagePage'; // Import the new ImagePage component
+import ImagePage from './pages/ImagePage';  // Import the dynamic ImagePage component
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/imagepage/:id" element={<ImagePage />} /> {/* Dynamic Route */}
+        <Route path="/imagepage/:id" element={<ImagePage />} />  {/* Dynamic route */}
       </Routes>
     </Router>
   );
