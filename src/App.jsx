f12c4a544c2dbd899e89cb8bf8a0e4e87804961b
Mozzam Shahid <mozzam@Mozzams-MacBook-Air.local>
2024-12-25 19:43:00 +0500

    adding files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import './App.css'
-import FirstSection from "./components/FirstSection";
-import LogoComp from "./components/LogoComp";
-import ContentSection from "./components/ContentSection";
-import AlternateContent from "./components/AlternateContent";
-import FifthSection from "./components/FifthSection";
-import SlideShow from "./components/SlideShow";
+import Header from "./components/Header";
+import Card from "./components/Card";
 function App() {
   return (
     <div className="ml-36 mr-36">
-      <FirstSection/>
-      <LogoComp/>
-      <ContentSection/>
-      <AlternateContent/>
-      <FifthSection/>
-      <SlideShow/>
+    <Header/>
+    <Card/>
     </div>
   );
 }
